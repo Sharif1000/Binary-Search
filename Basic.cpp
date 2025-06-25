236517dc9e5778dd56e64c5f955b7491802646c7
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-06-25 15:50:28 +0600

    Update Basic.cpp

--- Basic.cpp
+++ Basic.cpp
@@ -1,81 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-int search(vector<int>& nums, int target) {
-        // Set the left and right boundaries
-        int left = 0, right = int(nums.size()) - 1;
-        
-        // Under this condition
-        while (left <= right) {
-            // Get the middle index and the middle value.
-            int mid = left + (right - left) / 2;
-            
-            // Case 1, return the middle index.
-            if (nums[mid] == target) {
-                return mid;
-            } 
-            // Case 2, discard the smaller half.
-            else if (nums[mid] < target) {
-                left = mid + 1;   
-            } 
-            // Case 3, discard the larger half.
-            else {
-                right = mid - 1;
-            }
-        }
-        
-        // If we finish the search without finding target, return -1.
-        return -1;
-    }
-    
-    
-    int searchup(vector<int>& nums, int target) {
-        // Set the left and right boundaries
-        int left = 0, right = int(nums.size());
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        
-        return left;
-    }
+int binarySearch(vector<int> v, int target){
+  int l = 0, r = v.size() - 1;
+  while(l <= r){
+    int mid = l + (r - l) / 2;
+    if(v[mid] == target) return mid;
+    else if(v[mid] < target) l = mid + 1;
+    else r = mid - 1;
+  }
+  return -1;
+}
 
 
-int mySqrt(int x) {
-        if(x==0)
-            return 0;
-        int lo=1,high=x,res;
-        uint64_t mid;
-        while(lo<=high)
-        {
-            mid=lo+(high-lo)/2;
-            if(mid*mid<=x)
-            {
-                lo=mid+1;
-                res=mid;
-            }
-            else
-                high=mid-1;
-        }
-        return res;
+int binarySearchlower(vector<int> v, int target){
+  int l = 0, r = v.size() - 1;
+  int ans = v.size();
+  while(l <= r){
+    int mid = l + (r - l) / 2;
+    if(v[mid] >= target){
+      ans = mid;
+      r = mid - 1;
     }
+    else l = mid + 1;
+  }
+  return ans;
+}
+
+int binarySearchupper(vector<int> v, int target){
+  int l = 0, r = v.size() - 1;
+  int ans = v.size();
+  while(l <= r){
+    int mid = l + (r - l) / 2;
+    if(v[mid] > target){
+      ans = mid;
+      r = mid - 1;
+    }
+    else l = mid + 1;
+  }
+  return ans;
+}
+
+int sqrtBinarySearch(int n) {
+    if (n == 0 || n == 1) return n;
+    int low = 1, high = n, ans = 0;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if ((long long)mid * mid <= n) {
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
 
 int main(){
-  int t; cin>>t;
-  while(t--){
-    int n, tar; cin>>n>>tar;
-    vector<int>v(n);
-    for(int i = 0; i < n; i++) cin>>v[i];
-    // int idx = searchup(v, tar);
-    // cout<<idx;
-    int idx = lower_bound(v.begin(), v.end(), tar) - v.begin();
-    cout<<idx;
-  }
+  int n, t; cin>>n>>t;
+  vector<int>v(n);
+  for(int i = 0; i < n; i++) cin>>v[i];
+  int res = binarySearchupper(v, t);
+  cout<<res;
   return 0;
 }
